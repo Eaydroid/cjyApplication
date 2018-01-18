d6e2d30c3af0e3a8a33e9c8b51d44ceb1289681a
cuijingyi <cuijingyi@miao.cn>
2018-01-18 14:42:56 +0800

    change MainActivity

--- app/src/main/java/com/more/cjy/MainActivity.java
+++ app/src/main/java/com/more/cjy/MainActivity.java
@@ -2,12 +2,26 @@ package com.more.cjy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button btn_circleBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_circleBar = (Button) findViewById(R.id.btn_circleBar);
+        btn_circleBar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_circleBar:
+                break;
+        }
     }
 }
